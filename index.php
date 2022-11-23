ca5667af1ca90af3679aa53aecb559bd9c120d93
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2022-11-23 14:19:06 +0100

    Initial structure

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receptenboek</title>
+    
+    <link href="./css/styles.css" rel="stylesheet">
+    
+    <script src="./scripts/main.js" defer></script>
+</head>
+
+
+<body>
+    <header>
+        
+    </header>
+</body>
+
+</html>
