42372f3967cd28b89ce025a35a2ae3d8581c30f2
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2022-11-23 16:06:18 +0100

    Initial styling recept

--- recept.php
+++ recept.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Receptenboek</title>
+    <title>Recept</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,400;1,300&display=swap" rel="stylesheet"> 
     
     <link href="./css/recept/styles.css" rel="stylesheet">
     
@@ -15,12 +19,45 @@
 <body>
     <header>
         <h1>Receptenboek</h1>
-        <div>Home</div>
-        <div>Contact</div>
+        <div>
+            <div>Home</div>
+            <div>Contact</div>
+        </div>
     </header>
     <main>
+        <section>
+            <div>
+                <h2>
+                    Generaal Tso's Chicken
+                </h2>
+                <div>
+                    <img src="https://stripedspatula.com/wp-content/uploads/2019/01/general-tsos-chicken-7.jpg" alt="">
+                    <ul>                
+                        <li>Kipfilet</li>
+                        <li>Maïzena</li>
+                        <li>Olie</li>
+                        <li>Sojasaus</li>
+                        <li>Rijstazijn</li>
+                        <li>Hoisinsaus</li>
+                        <li>Bruine suiker</li>
+                        <li>Kippen bouillon</li>
+                        <li>Gemberpasta</li>
+                        <li>Knoflook poeder</li>
+                        <li>Gedroogde of vermalen rode peper</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section>
 
+        </section>
     </main>
+    <footer>
+        <div>
+            <div>Home</div>
+            <div>Contact</div>
+        </div>
+    </footer>
 </body>
 
 </html>
