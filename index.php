eadf98cb6ff0dbdd6a6e4b72d5f010a3733991dc
rogerfate <rogersnelders2001@gmail.com>
2022-11-25 12:23:51 +0100

    Front_page_v2

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Receptenboek</title>
-    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;1,300&display=swap" rel="stylesheet">
     <link href="./css/home/styles.css" rel="stylesheet">
     
     <script src="./scripts/main.js" defer></script>
@@ -24,14 +26,14 @@
                 <h1>Welkom bij,<br> Receptenboek</h1>
             </div>
         </div>
-        <div class="tempclass">
+        <div class="aboutUs">
             <h2>About us</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ipsa. Delectus sint placeat optio quidem dolore error sapiente similique consectetur illo, qui quibusdam corrupti veniam architecto amet blanditiis totam alias, provident reprehenderit, ab quod eum. Autem, facere! Maxime, voluptatum voluptate voluptatem qui blanditiis aperiam fuga assumenda. Ea commodi quaerat, architecto ducimus maiores error quibusdam deleniti nihil dolorem consequatur reiciendis, laborum exercitationem est animi? Voluptates molestias officiis atque quisquam natus veniam suscipit? Ducimus iusto laborum totam.</p>
         </div>
         <ul>
             <li>
-                <div class="placeholderdiv">
-                    <h3>placeholdername</h3>
+                <div class="recipe chicken">
+                    <h3>General Tso's Chicken</h3>
                 </div>
                 <div>
                     <h2>Placeholder recipe</h2>
@@ -39,8 +41,8 @@
                 </div>
             </li>
             <li>
-                <div class="placeholderdiv">
-                    <h3>placeholdername</h3>
+                <div class="recipe fish">
+                    <h3>Fish</h3>
                 </div>
                 <div>
                     <h2>Placeholder recipe</h2>
@@ -48,8 +50,8 @@
                 </div>
             </li>
             <li>
-                <div class="placeholderdiv">
-                    <h3>placeholdername</h3>
+                <div class="recipe rice">
+                    <h3>Rijst</h3>
                 </div>
                 <div>
                     <h2>Placeholder recipe</h2>
