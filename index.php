82887f76bfc4abd81547c56f31f735d110f5cb49
rogerfate <rogersnelders2001@gmail.com>
2022-11-25 11:36:37 +0100

    Home_page_styling_v1

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
     
     <script src="./scripts/main.js" defer></script>
 </head>
-
-
 <body>
     <header>
         <h1>Recepten</h1>
@@ -26,12 +24,39 @@
                 <h1>Welkom bij,<br> Receptenboek</h1>
             </div>
         </div>
-        <div>
-            <div>
-                <div class="placeholderdiv"></div>
-                <h2>Placeholder recipe</h2>
-            </div>
+        <div class="tempclass">
+            <h2>About us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, ipsa. Delectus sint placeat optio quidem dolore error sapiente similique consectetur illo, qui quibusdam corrupti veniam architecto amet blanditiis totam alias, provident reprehenderit, ab quod eum. Autem, facere! Maxime, voluptatum voluptate voluptatem qui blanditiis aperiam fuga assumenda. Ea commodi quaerat, architecto ducimus maiores error quibusdam deleniti nihil dolorem consequatur reiciendis, laborum exercitationem est animi? Voluptates molestias officiis atque quisquam natus veniam suscipit? Ducimus iusto laborum totam.</p>
         </div>
+        <ul>
+            <li>
+                <div class="placeholderdiv">
+                    <h3>placeholdername</h3>
+                </div>
+                <div>
+                    <h2>Placeholder recipe</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore adipisci natus quibusdam, aut quis harum. Dolores unde quidem consequuntur voluptatibus.</p>
+                </div>
+            </li>
+            <li>
+                <div class="placeholderdiv">
+                    <h3>placeholdername</h3>
+                </div>
+                <div>
+                    <h2>Placeholder recipe</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore adipisci natus quibusdam, aut quis harum. Dolores unde quidem consequuntur voluptatibus.</p>
+                </div>
+            </li>
+            <li>
+                <div class="placeholderdiv">
+                    <h3>placeholdername</h3>
+                </div>
+                <div>
+                    <h2>Placeholder recipe</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore adipisci natus quibusdam, aut quis harum. Dolores unde quidem consequuntur voluptatibus.</p>
+                </div>
+            </li>
+        </ul>
     </main>
     <footer>
         <div>
@@ -39,6 +64,7 @@
             <div>Contact</div>
         </div>
     </footer>
+
 </body>
 
 </html>
