d79217f245121f2e9f0baac4456c58a17262f376
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2022-11-25 12:23:17 +0100

    Initial styling

--- recept.php
+++ recept.php
@@ -25,31 +25,65 @@
         </div>
     </header>
     <main>
-        <section>
+        <section id="section1">
             <div>
+                <img src="https://stripedspatula.com/wp-content/uploads/2019/01/general-tsos-chicken-social.jpg" alt="">
                 <h2>
-                    Generaal Tso's Chicken
+                    General Tso's Chicken
                 </h2>
-                <div>
-                    <img src="https://stripedspatula.com/wp-content/uploads/2019/01/general-tsos-chicken-7.jpg" alt="">
-                    <ul>                
-                        <li>Kipfilet</li>
-                        <li>Maïzena</li>
-                        <li>Olie</li>
-                        <li>Sojasaus</li>
-                        <li>Rijstazijn</li>
-                        <li>Hoisinsaus</li>
-                        <li>Bruine suiker</li>
-                        <li>Kippen bouillon</li>
-                        <li>Gemberpasta</li>
-                        <li>Knoflook poeder</li>
-                        <li>Gedroogde of vermalen rode peper</li>
-                    </ul>
-                </div>
             </div>
         </section>
-        <section>
-
+        <section id="section2">
+            <div>
+                <div>
+                    <img width="1036px" height="542px" src="https://stripedspatula.com/wp-content/uploads/2019/01/general-tsos-chicken-7.jpg" alt="">
+                    <div>
+                        <h3>Benodigdheden:</h3>
+                        <ul>                
+                            <li>Kipfilet</li>
+                            <span></span>
+                            <li>Maïzena</li>
+                            <span></span>
+                            <li>Olie</li>
+                            <span></span>
+                            <li>Sojasaus</li>
+                            <span></span>
+                            <li>Rijstazijn</li>
+                            <span></span>
+                            <li>Hoisinsaus</li>
+                            <span></span>
+                            <li>Bruine suiker</li>
+                            <span></span>
+                            <li>Kippen bouillon</li>
+                            <span></span>
+                            <li>Gemberpasta</li>
+                            <span></span>
+                            <li>Knoflook poeder</li>
+                            <span></span>
+                            <li>Gedroogde of vermalen rode peper</li>
+                            <span></span>
+                        </ul>
+                    </div>
+                </div>
+                <span class="section-separator"></span>
+            </div>
+        </section>
+        <section id="section3">
+            <div>
+                <div>
+                    <h3>Bereidingswijze:</h3>
+                    <ol>
+                        <li>Klop 4 eetlepels sojasaus, 4 eetlepels rijstazijn, 4 eetlepels hoisinsaus, 4 eetlepels bruine suiker, 1 eetlepel gemberpasta en ¼ kopje kippenbouillon samen in een kom. Opzij zetten.</li>
+                        <li>Voeg ¼ kopje maizena toe aan een kleine kom. Gooi de stukjes kip in het maizena tot ze volledig bedekt zijn. Laat ze voorlopig in de kom.</li>
+                        <li>Zet je brander op middelhoog vuur. Verhit de olie die je gaat gebruiken om te bakken in een grote koekenpan. Bak een paar stukjes van de gepaneerde kip tegelijk. Als ze klaar zijn, zijn ze goudbruin, krokant en goed gaar. Zet opzij om uit te lekken.</li>
+                        <li>Als het laatste stuk kip klaar is, verwijder dan alles behalve ongeveer 1 eetlepel olie uit de pan.</li>
+                        <li>Giet de kom met de geklopte sausingrediënten in de pan en breng het aan de kook. Blijf de saus op dit vuur koken tot hij dikker wordt, af en toe roeren om aanbranden te voorkomen.</li>
+                        <li>Als de saus is ingedikt, doe je de stukjes kip terug in de pan. Gooi om de krokant gebakken kip grondig te coaten voor het serveren.</li>
+                        <li>Haal de pan van het vuur en giet de mix over rijst om te serveren. Garneer met gehakte groene uien of sesamzaadjes indien gewenst.</li>
+                    </ol>
+                </div>
+                <span class="section-separator"></span>
+            </div>
         </section>
     </main>
     <footer>
