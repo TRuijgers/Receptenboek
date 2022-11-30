6a37e0b0952508eed2c71f10409fd7c06eeb98e2
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2022-11-30 16:15:56 +0100

    WIP content functions

--- recipebook/content.php
+++ recipebook/content.php
@@ -2,7 +2,26 @@
 require_once('./recipebook/recipebook.php');
 
 class LoadContent {
-    public function getRecipe(){
-        
+    public function printRecipe(){
+        fetchJoinedData($id, $table);
+
+    }
+    public function printIngredients($id){
+
+        Recipebook::fetchJoinedData($id, $table);
+        foreach ($ingredients as $key=>$value){
+            echo "<li>${value['name']}</li><span class='li-separator'></span>";
+        }
+    }
+    public function printPreparationSteps($id){
+        $table = 'preparation';
+        Recipebook::fetchJoinedData($id, $table);
+
+        foreach ($preparation as $key=>$value){
+            echo "<li>${value['description']}</li><span class='li-separator'></span>";
+        }
+    }
+    public function printImagePaths($id, $position){
+        Recipebook::fetchJoinedData($id, $table);
     }
 }
--- recipebook/recipebook.php
+++ recipebook/recipebook.php
@@ -18,20 +18,6 @@ class Recipebook {
  
         return $statement->fetchAll();
     }
-    // public function fetchData(int $id, string $table) : array{
-    //     if ($table == 'preparation' ||$table == 'images'){
-    //         $query = "SELECT * FROM `${table}` WHERE `recipes_id` = :id ORDER BY `${table}`.`sequence` ASC";
-    //     } else {
-    //         $query = "SELECT * FROM `${table}` WHERE `recipes_id` = :id";
-    //     }
-    //     $statement = $this->pdo->prepare($query);
-
-    //     $statement->execute(array(
-    //         ':id' => $id
-    //     ));
-
-    //     return $statement->fetchAll();
-    // }
     public function fetchJoinedData(int $id, string $table) : array{
         $b_table = 'recipes_' . $table;
         $column = $table . '_id';
