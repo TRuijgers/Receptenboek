3431a0062ea3e16470221125b8fc1d7ab79c2c9c
TRuijgers <26258112+TRuijgers@users.noreply.github.com>
2022-11-30 09:15:14 +0100

    Removed unnecessary commented functions

--- recipebook/recipebook.php
+++ recipebook/recipebook.php
@@ -14,7 +14,7 @@ class Recipebook {
 
         $statement->execute(array(
             ':id' => $id
-         ));
+        ));
  
         return $statement->fetchAll();
     }
@@ -32,49 +32,5 @@ class Recipebook {
 
         return $statement->fetchAll();
     }
-    // public function fetchIngredients(int $id) : array{
-    //     $query = "SELECT * FROM `ingredients` WHERE `recipes_id` = :id";
-
-    //     $statement = $this->pdo->prepare($query);
-
-    //     $statement->execute(array(
-    //         ':id' => $id
-    //      ));
- 
-    //     return $statement->fetchAll();
-    // }
-    // public function fetchTools(int $id) : array{
-    //     $query = "SELECT * FROM `tools` WHERE `recipes_id` = :id";
-
-    //     $statement = $this->pdo->prepare($query);
-
-    //     $statement->execute(array(
-    //         ':id' => $id
-    //      ));
- 
-    //     return $statement->fetchAll();
-    // }
-    // public function fetchPreparation(int $id) : array{
-    //     $query = "SELECT * FROM `preparation` WHERE `recipes_id` = :id ORDER BY `sequence` ASC";
-
-    //     $statement = $this->pdo->prepare($query);
-
-    //     $statement->execute(array(
-    //         ':id' => $id
-    //      ));
- 
-    //     return $statement->fetchAll();
-    // }
-    // public function fetchImages(int $id) : array{
-    //     $query = "SELECT path FROM `images` WHERE `recipes_id` = :id ORDER BY `sequence` ASC";
-
-    //     $statement = $this->pdo->prepare($query);
-
-    //     $statement->execute(array(
-    //         ':id' => $id
-    //      ));
- 
-    //     return $statement->fetchAll();
-    // }
 }
 
