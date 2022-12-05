92197f5830188ccaef8e29d891b32de1f1888dd3
Thomas Ruijgers <26258112+TRuijgers@users.noreply.github.com>
2022-12-05 23:14:56 +0100

    Responsive design appears complete

--- components/section_1.php
+++ components/section_1.php
@@ -1,16 +1,17 @@
 <?php ?>
     <section id="section1">
         <div class="primary-div-right">
-            <div class="text-container">
-                <p><?php $contentDistributor->printRecipeContent('description')?></p>
-            </div>
+
             <div class="bundler">
+                <div class="text-container">
+                    <?php $contentDistributor->printDescription()?>
+                </div>
                 <div class="image-container">
                     <img src=<?php $contentDistributor->printImage(0); ?> alt="">
                 </div>
-                <div class="title-container">
-                    <h2><?php $contentDistributor->printRecipeContent('title'); ?></h2>
-                </div>
+            </div>
+            <div class="title-container">
+                <h2><?php $contentDistributor->printRecipeContent('title'); ?></h2>
             </div>
         </div>
         <div class="secondary-div">
--- components/section_2.php
+++ components/section_2.php
@@ -5,21 +5,21 @@
                 <div class="image-container">
                     <img src=<?php $contentDistributor->printImage(1); ?> alt="">
                 </div>
-                <div class="title-container">
-                    <h3>Benodigdheden</h3>
+                <div id="option-container">
+                    <div class="quantity-option">
+                        <button id="plus">&plus;</button>
+                        <button id="minus">&minus;</button>
+                    </div>
+                    <div class="quantity-text">
+                        <p>
+                            <span id="quantity-display"><?php $contentDistributor->printRecipeContent('quantity');?></span>
+                            <span><?php $contentDistributor->printRecipeContent('unit');?></span>
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div id="option-container">
-                <div class="quantity-option">
-                    <button id="plus">&plus;</button>
-                    <button id="minus">&minus;</button>
-                </div>
-                <div class="quantity-text">
-                    <p>
-                        <span id="quantity-display"><?php $contentDistributor->printRecipeContent('quantity');?></span>
-                        <span><?php $contentDistributor->printRecipeContent('unit');?></span>
-                    </p>
-                </div>
+            <div class="title-container">
+                <h3>Benodigdheden</h3>
             </div>
         </div>
         <div class="secondary-div">
